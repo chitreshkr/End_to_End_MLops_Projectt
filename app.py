5bbd4dbf8e97bf90b26e0bc1e8e8619084b11ddc
Chitresh Kumar <chitreshkumar@Chitreshs-Laptop.local>
2024-06-18 00:16:51 +0530

    Folder Structure added

---
+++ app.py
@@ -0,0 +1,9 @@
+##Train our model
+## Create Web App using Flask
+##Commit the code in Github
+##Create an Account in Heroku
+##Link the Github to Heroku
+##Deploy the model
+##Web App is ready
+
+import numpy as np
